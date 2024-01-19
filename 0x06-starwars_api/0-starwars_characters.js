26af6b4a6253bb98520c65ffa985fb87aaf59902
dancan-agweli <agwelidancan32@gmail.com>
2024-01-19 09:29:51 +0000

    starwas

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,51 +1,25 @@
-const axios = require('axios');
+#!/usr/bin/node
+// A script that prints all characters of a Star Wars movie
 
-const getMovieCharacters = async (movieId) => {
-  try {
-    // Fetch movie data from SWAPI
-    const movieResponse = await axios.get(`https://swapi.dev/api/films/${movieId}/`);
-    const movieData = movieResponse.data;
+const request = require('request');
 
-    if (!movieData || movieData.detail === 'Not found') {
-      console.log(`Movie with ID ${movieId} not found.`);
-      return;
-    }
-
-    const charactersUrls = movieData.characters;
-
-    // Fetch character names from SWAPI
-    const characterNames = await Promise.all(
-      charactersUrls.map(async (characterUrl) => {
-        const characterResponse = await axios.get(characterUrl);
-        return characterResponse.data.name;
-      })
-    );
-
-    return characterNames;
-  } catch (error) {
-    console.error(`Error fetching data from SWAPI: ${error.message}`);
+const movie = process.argv[2];
+const api = 'https://swapi-api.hbtn.io/api/';
+const url = api + 'films/' + movie + '/';
+request.get({ url: url }, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    order(characters);
   }
-};
+});
 
-const main = async () => {
-  const args = process.argv.slice(2);
-
-  if (args.length !== 1 || isNaN(args[0])) {
-    console.log('Usage: node 0-starwars_characters.js <movie_id>');
-    process.exit(1);
-  }
-
-  const movieId = parseInt(args[0], 10);
-
-  const characterNames = await getMovieCharacters(movieId);
-
-  if (characterNames) {
-    console.log(`Characters in Star Wars Episode ${movieId}:`);
-    characterNames.forEach((characterName) => {
-      console.log(characterName);
+function order (characters) {
+  if (characters.length > 0) {
+    request.get({ url: characters.shift() }, function (err, res, body) {
+      if (!err) {
+        console.log(JSON.parse(body).name);
+        order(characters);
+      }
     });
   }
-};
-
-main();
-
+}
